68451b53bfce5eca32936e9ee38a5cece4a666b7
hojat <hojat72elect@gmail.com>
2024-10-09 10:50:17 -0400

    Settings.java -> kt

--- core/src/com/nopalsoft/dragracer/Settings.kt
+++ core/src/com/nopalsoft/dragracer/Settings.kt
@@ -1,51 +1,69 @@
-package com.nopalsoft.dragracer;
+package com.nopalsoft.dragracer
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Preferences;
+import com.badlogic.gdx.Gdx
+import com.nopalsoft.dragracer.shop.CharactersSubMenu
 
-public class Settings {
+object Settings {
 
-    final public static int TIMES_TO_SHOW_AD = 5;
-    private final static Preferences pref = Gdx.app.getPreferences("com.tiar.dragrace.shop");
-    public static boolean drawDebugLines = false;
-    public static int numberOfTimesPlayed = 0;
-    public static int bestScore = 0;
-    public static int coinsTotal = 0;
-    public static boolean didBuyNoAds;
-    public static boolean didLikeFacebook;
-    public static boolean isMusicOn = true;
-    public static int selectedSkin = com.nopalsoft.dragracer.shop.CharactersSubMenu.SKIN_CAR_DEVIL;
+    const val TIMES_TO_SHOW_AD = 5
 
-    public static void load() {
-        numberOfTimesPlayed = pref.getInteger("numeroVecesJugadas");
-        bestScore = pref.getInteger("bestScore");
-        coinsTotal = pref.getInteger("coinsTotal");
-        selectedSkin = pref.getInteger("skinSeleccionada");
+    private val pref = Gdx.app.getPreferences("com.tiar.dragrace.shop")
 
-        didBuyNoAds = pref.getBoolean("didBuyNoAds");
-        didLikeFacebook = pref.getBoolean("didLikeFacebook");
-        isMusicOn = pref.getBoolean("isMusicOn", true);
+    @JvmField
+    val drawDebugLines = false
 
+    @JvmField
+    var numberOfTimesPlayed = 0
+
+    @JvmField
+    var bestScore = 0
+
+    @JvmField
+    var coinsTotal = 0
+
+    @JvmField
+    var didBuyNoAds = false
+
+    @JvmField
+    var didLikeFacebook = false
+
+    @JvmField
+    var isMusicOn = true
+
+    @JvmField
+    var selectedSkin = CharactersSubMenu.SKIN_CAR_DEVIL
+
+    @JvmStatic
+    fun load() {
+        numberOfTimesPlayed = pref.getInteger("numeroVecesJugadas")
+        bestScore = pref.getInteger("bestScore")
+        coinsTotal = pref.getInteger("coinsTotal")
+        selectedSkin = pref.getInteger("skinSeleccionada")
+
+        didBuyNoAds = pref.getBoolean("didBuyNoAds")
+        didLikeFacebook = pref.getBoolean("didLikeFacebook")
+        isMusicOn = pref.getBoolean("isMusicOn", true)
     }
 
-    public static void save() {
-        pref.putInteger("numeroVecesJugadas", numberOfTimesPlayed);
-        pref.putInteger("bestScore", bestScore);
-        pref.putInteger("coinsTotal", coinsTotal);
-        pref.putInteger("skinSeleccionada", selectedSkin);
+    @JvmStatic
+    fun save() {
+        pref.putInteger("numeroVecesJugadas", numberOfTimesPlayed)
+        pref.putInteger("bestScore", bestScore)
+        pref.putInteger("coinsTotal", coinsTotal)
+        pref.putInteger("skinSeleccionada", selectedSkin)
 
-        pref.putBoolean("didBuyNoAds", didBuyNoAds);
-        pref.putBoolean("didLikeFacebook", didLikeFacebook);
-        pref.putBoolean("isMusicOn", isMusicOn);
-        pref.flush();
-
+        pref.putBoolean("didBuyNoAds", didBuyNoAds)
+        pref.putBoolean("didLikeFacebook", didLikeFacebook)
+        pref.putBoolean("isMusicOn", isMusicOn)
+        pref.flush()
     }
 
-
-    public static void setNewScore(int score) {
-        if (bestScore < score)
-            bestScore = score;
-        save();
+    @JvmStatic
+    fun setNewScore(score: Int) {
+        if (bestScore < score) {
+            bestScore = score
+        }
+        save()
     }
 
 }
